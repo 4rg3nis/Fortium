c736f09dc82e29f045f0ac14e89bee4a510242c1
Argenis <argenislorabautista@gmail.com>
2026-03-31 17:37:26 +0200

    feat: add Entity's and Enum's without Dao's

---
+++ app/src/main/java/com/sthenos/fortium/model/entities/Usuario.java
@@ -0,0 +1,79 @@
+package com.sthenos.fortium.model.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import com.sthenos.fortium.model.enums.UnitMeasure;
+
+@Entity
+public class Usuario {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name="nombre")
+    @NonNull
+    private String nombre;
+    @ColumnInfo(name="apellido")
+    private String apellido;
+    @ColumnInfo(name="fechaNacimiento")
+    private String fechaNacimiento;
+    @ColumnInfo(name="pesoActual")
+    private String pesoActual;
+    @ColumnInfo(name="genero")
+    private String genero;
+    @ColumnInfo(name="altura")
+    private String altura;
+    @ColumnInfo(name="unidadMedida")
+    private UnitMeasure unidadmedida;
+
+    public Usuario(int uid, String nombre, String apellido, String fechaNacimiento, String pesoActual, String genero, String altura, UnitMeasure unidadmedida) {
+        this.id = uid;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.pesoActual = pesoActual;
+        this.genero = genero;
+        this.altura = altura;
+        this.unidadmedida = unidadmedida;
+    }
+
+    public Usuario(String nombre, String apellido, String fechaNacimiento, String pesoActual, String genero, String altura, UnitMeasure unidadmedida) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+        this.pesoActual = pesoActual;
+        this.genero = genero;
+        this.altura = altura;
+        this.unidadmedida = unidadmedida;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public String getPesoActual() {
+        return pesoActual;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public UnitMeasure getUnidadmedida() {
+        return unidadmedida;
+    }
+}
---
+++ app/src/main/java/com/sthenos/fortium/model/enums/Equipo.java
@@ -0,0 +1,8 @@
+package com.sthenos.fortium.model.enums;
+
+public enum Equipo {
+    MAQUINA,
+    PESO_LIBRE,
+    PESO_CORPORAL,
+    POLEA
+}
