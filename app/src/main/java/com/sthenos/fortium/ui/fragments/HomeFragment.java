fe26f9b41b7694128f6c1ac30c6c7ac105d3c31e
Argenis <argenislorabautista@gmail.com>
2026-04-18 17:51:54 +0200

    fix: Quitar ciertas coassa

--- app/src/main/java/com/sthenos/fortium/ui/fragments/HomeFragment.java
+++ app/src/main/java/com/sthenos/fortium/ui/fragments/HomeFragment.java
@@ -22,17 +22,7 @@ import com.sthenos.fortium.ui.adapters.RutinaAdapter;
 import com.sthenos.fortium.ui.viewmodels.RutinaViewModel;
 import com.sthenos.fortium.ui.viewmodels.UsuarioViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     private TextView tvSaludo, tvPeso, tvRm;
     private RecyclerView rvRutinas;
@@ -41,31 +31,8 @@ public class HomeFragment extends Fragment {
     private UsuarioViewModel usuarioViewModel;
     private int idUsuairo = -1;
 
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public HomeFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
@@ -98,7 +65,7 @@ public class HomeFragment extends Fragment {
         rutinaViewModel.getAllRutinas().observe(getViewLifecycleOwner(), rutinas -> {
             ((RutinaAdapter) rvRutinas.getAdapter()).setRutinas(rutinas);
         });
-        usuarioViewModel.getUsuarioActual().observe(this, usuario -> {
+        usuarioViewModel.getUsuarioActual().observe(getViewLifecycleOwner(), usuario -> {
             idUsuairo = usuario.getId();
             setPeso(usuario.getPesoActual());
             setSaludo(usuario.getNombre());
